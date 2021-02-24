3c46e0f72c0cdd66080d7928d873d33592391195
Roman Stashchak <stashchak90@gmail.com>
2021-02-24 14:42:19 +0200

    Add files via upload

---
+++ les3/messages/error.messages.js
@@ -0,0 +1,22 @@
+module.exports = {
+    TOO_WEAK_PASSWORD: {
+        en: 'Too weak password',
+        ua: 'Пароль залегкий',
+		de: 'Passwort ist zu einfach'
+    },
+	USER_NOT_CREATED: {
+		en: 'User is not created',
+        ua: 'Користувача не створено',
+		de: 'Benutzer wurde nicht erstellt'
+    },
+	USER_NOT_FOUND: {
+		en: 'User is not found',
+        ua: 'Користувача не знайдено',
+		de: 'Benutzer wurde nicht gefunden'
+    },
+	USER_IS_EXIST: {
+		en: 'User is already exist',
+        ua: 'Користувач вже існує',
+		de: 'Benutzer bereits existiert'
+    },
+}

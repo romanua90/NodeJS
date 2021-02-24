bba6be41c54fef924236244a4cb170389eceb99d
Roman Stashchak <stashchak90@gmail.com>
2021-02-24 15:24:41 +0200

    Update user.router.js

--- les3/router/user.router.js
+++ les3/router/user.router.js
@@ -1,14 +1,15 @@
 const router=require('express').Router();
 
 const userController=require('../controller/user.controller');
+const userMiddleware = require('../middleware/user.middleware');
 
 router.get('/', userController.getAllUsers);
 
 
 router.get('/:userId', userController.getSingleUser);
 
-router.delete('/:userId', userController.deleteUser);
+router.delete('/:userId', userMiddleware.checkIsIdValid, userController.deleteUser);
 
-router.post('/', userController.createUser);
+router.post('/', userMiddleware.isUserValid, userController.createUser);
 
 module.exports=router;

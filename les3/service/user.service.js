d48a029da1f0fcea4f167e2f4aca44328555ddd8
Roman Stashchak <stashchak90@gmail.com>
2021-02-24 15:19:17 +0200

    Update user.service.js

--- les3/service/user.service.js
+++ les3/service/user.service.js
@@ -1,21 +1,38 @@
 const path = require('path');
 const { promisify } = require('util');
 const fs = require('fs');
+const errorMessages = require('../messages/error.messages');
+
 const dbPath = path.join(process.cwd(), 'dataBase', 'users.json')
+
 const readFilePromise = promisify(fs.readFile);
 const writeFilePromise = promisify(fs.writeFile);
 
 
+/**
+*@ findUsers - this is function to find all existing users
+*@ findUserById - this is function to find user by id
+*@ createUser - this is function for creating new user
+*@ deleteUser - this is function for deleting selected user
+**/
+
 module.exports = {
-    findUsers: async () => {
+    findUsers: async (prefLang) => {
         const DB = await readFilePromise(dbPath);
         return JSON.parse(DB.toString());
     },
 
 
-    findUserById: async (userId) => {
+    findUserById: async (userId, prefLang) => {
         const DB = await readFilePromise(dbPath);
         const users = JSON.parse(DB.toString());
+        
+        const found = users.find(user => user.userId === userId);
+    
+         if (!found) {
+            throw new Error(errorMessages.USER_NOT_FOUND[prefLang]);
+         }
+        
         return users[userId];
     },
 

6c437a792cacb5e7c7c13277cdc13c5686adb7c8
Roman Stashchak <stashchak90@gmail.com>
2021-02-24 14:09:23 +0200

    fix unnecessary code

--- les3/app.js
+++ les3/app.js
@@ -1,14 +1,8 @@
-const fs=require('fs');
 const express=require('express')
-const path=require('path');
 
 const apiRouter=require('./router/api.router')
 
 const app=express();
-
-
-
-const usersPath = path.join(__dirname, 'dataBase', 'users.json');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));

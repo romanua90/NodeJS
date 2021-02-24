4c59e33553bb6a539e035daf5f0b34761c10f98c
Roman Stashchak <stashchak90@gmail.com>
2021-02-24 14:57:58 +0200

    Update user.controller.js

--- les3/controller/user.controller.js
+++ les3/controller/user.controller.js
@@ -1,28 +1,61 @@
 const userService = require('../service/user.service');
+const errorCodes = require('../constant/errorCodes.enum.js');
 
 module.exports = {
     getAllUsers: async (req, res) => {
-        const users = await userService.findUsers();
+        try{
+            
+        const {body: {prefLang = 'de'}, query} = req;
+            
+        const users = await userService.findUsers(query, prefLang);
+            
         res.json(users);
+        }
+        catch (err){
+            
+        res.status(errorCodes.BAD_REQUEST).json(err.message);
+            
+        }
     },
 
     getSingleUser: async (req, res) => {
+        try{
+            
         const {userId} = req.params;
+            
         const user = await userService.findUserById(userId);
 
         res.json(user);
+        }
+        catch (err) {
+            
+         res.status(errorCodes.BAD_REQUEST).json(err.message);
+            
+        }
     },
     createUser: async (req, res) => {
+        try {
+            
         const user = req.body;
 
         await userService.createUser(user);
 
         res.status(201).json('USER IS CREATED!');
+        }
+        catch (err){
+         res.status(errorCodes.BAD_REQUEST).json(err.message);
+        }
     },
     deleteUser: async (req, res) => {
+        try {
         const {userId} = req.params;
+        
         const user = await userService.deleteUser(userId);
 
         res.json(user);
+        }
+        catch (err){
+          res.status(errorCodes.BAD_REQUEST).json(err.message);   
+        }
     }
 }
